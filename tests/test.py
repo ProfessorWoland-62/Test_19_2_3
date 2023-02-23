fdd394eb84046720826c2ed11880130c7b93e390
ProfessorWoland-62 <sokurenkorzn@mail.ru>
2023-02-23 15:18:14 +0300

    Test 19.2.3

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,18 @@
-import pytest, os
+from app.calc import Calculator
 
-appPath = os.path.join('..', 'app', 'calc.py')
-print(appPath)
-from appPath import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def testMulPositive(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def testDivPositive(self):
+        assert self.calc.division(self, 10, 5) == 2
+
+    def testSubPositive(self):
+        assert self.calc.subtraction(self, 10, 5) == 5
+
+    def testAddPositive(self):
+        assert self.calc.adding(self, 10, 5) == 15
